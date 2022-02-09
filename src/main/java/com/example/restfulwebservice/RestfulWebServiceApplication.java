e9e5677fb2af891b908384f29742674fc09ed3a4
Lee JaeGeun <ljg95924@github.com>
2022-02-09 19:24:55 +0900

    다국어 처리를 위한 Internationalization 구현

--- src/main/java/com/example/restfulwebservice/RestfulWebServiceApplication.java
+++ src/main/java/com/example/restfulwebservice/RestfulWebServiceApplication.java
@@ -1,7 +1,13 @@
 package com.example.restfulwebservice;
 
+import org.hibernate.Session;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class RestfulWebServiceApplication {
@@ -10,4 +16,10 @@ public class RestfulWebServiceApplication {
 		SpringApplication.run(RestfulWebServiceApplication.class, args);
 	}
 
+	@Bean
+	public LocaleResolver localeResolver(){
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.KOREA);
+		return localeResolver;
+	}
 }
